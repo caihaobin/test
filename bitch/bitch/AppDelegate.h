44f24929a6837ebe3a3b3847787d30e0d28940fa
浩彬蔡 <haobincai@haobindemacbook-air.local>
2015-12-29 22:58:50 +0800

    this is bitch

---
+++ bitch/bitch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  bitch
+//
+//  Created by 浩彬 蔡 on 15/12/29.
+//  Copyright © 2015年 浩彬 蔡. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ bitch/bitch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  bitch
+//
+//  Created by 浩彬 蔡 on 15/12/29.
+//  Copyright © 2015年 浩彬 蔡. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
